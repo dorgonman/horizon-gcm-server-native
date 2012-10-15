b8e368a272a26cb50e94280e8459be949b7d284e
dorgonman <dorgonman@hotmail.com>
2012-10-15 21:10:28 +0800

    update
    Signed-off-by: dorgonman <dorgonman@hotmail.com>

--- src/GcmServer.java
+++ src/GcmServer.java
@@ -5,7 +5,7 @@ import org.json.simple.parser.JSONParser;
 public class GcmServer {
 
 	
-	public static String REGISTRATION_ID = "APA91bHCbHbeHuaXO-5zBToxEAlID67i9lXp_5MTeLTm-nXeZprZp_mw-RTNVffex1h11IsdQcT6LrUZ6QDEahsI0SyasSOMzL35DkzwgY_T016lDBDkKmg5mwVXnrv6TfkVyRK6zAgnQ4DHL500YglKJojsNiHNZg";
+	public static String REGISTRATION_ID = "APA91bGJwnRgYbdPozptxqyLXXHRn7KOomMn2Vtpysa1BUAC0Ywr3fRvqNqvcsxz5vnV3UDFTpG1q2WCiUDsjgtg7mxF-KgFvpLcewH_PYG5fJELvwkcE4QDw8IHq7EcNk9ZP9pAYaj-p_aPj9z_LUS75b3q7x5nzA";
 	public final static String GCM_API_KEY_SERVER = "AIzaSyA-cxSe50B1bdZ_IY8MB3uJZDAQAQSO1fk";
 	public static void main(String args[]) {
 		System.out.println("GcmServer");
@@ -19,14 +19,14 @@ public class GcmServer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		System.out.println(result.getMessageId());
+		System.out.println("result" + result.getMessageId());
 		
 		if (result.getMessageId() != null) {
 			 String canonicalRegId = result.getCanonicalRegistrationId();
 			
 			 if (canonicalRegId != null) {
 			   // same device has more than on registration ID: update database
-				 System.out.println("canonicalRegId:" + canonicalRegId);
+				 System.out.println("new canonicalRegId:" + canonicalRegId);
 			 }
 		} else {
 			 String error = result.getErrorCodeName();
